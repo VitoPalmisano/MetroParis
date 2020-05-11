de484511d9ee76dcf2a8ca55a6f8ef71b365c7cc
Vito <Vito@LAPTOP-B1E0VK6V.lan>
2020-05-11 16:48:28 +0200

    Cammino minimo

--- src/main/java/it/polito/tdp/metroparis/model/Model.java
+++ src/main/java/it/polito/tdp/metroparis/model/Model.java
@@ -6,7 +6,9 @@ import java.util.List;
 import java.util.Map;
 
 import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
 import org.jgrapht.Graphs;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.event.ConnectedComponentTraversalEvent;
 import org.jgrapht.event.EdgeTraversalEvent;
 import org.jgrapht.event.TraversalListener;
@@ -157,16 +159,27 @@ public class Model {
 		return albero;
 	}
 	
+	public List<Fermata> camminiMinimi(Fermata partenza, Fermata arrivo) {
+		DijkstraShortestPath<Fermata, DefaultEdge> dij = new DijkstraShortestPath<Fermata, DefaultEdge>(graph);
+		
+		GraphPath<Fermata, DefaultEdge> cammino = dij.getPath(partenza, arrivo);
+		
+		return cammino.getVertexList();
+	}
+	
 	public static void main(String args[]) {
 		Model m = new Model();
 		List<Fermata> visita1 = m.visitaAmpiezza(m.fermate.get(0));
-		System.out.println(visita1);
+//		System.out.println(visita1);
 		List<Fermata> visita2 = m.visitaProfondita(m.fermate.get(0));
-		System.out.println(visita2);
+//		System.out.println(visita2);
 		
 		Map<Fermata, Fermata> albero = m.alberoVisita(m.fermate.get(0));
 		for(Fermata f : albero.keySet()) {
-			System.out.format( "%s -> %s\n", f, albero.get(f));
+//			System.out.format( "%s -> %s\n", f, albero.get(f));
 		}
+		
+		List<Fermata> cammino = m.camminiMinimi(m.fermate.get(0), m.fermate.get(1));
+		System.out.println(cammino);
 	}
 }
